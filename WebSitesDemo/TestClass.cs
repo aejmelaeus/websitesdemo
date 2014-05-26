1b95f7052b96ab568bdfd45852cca1786750b40c
Weikko Aejmelaeus <weikko.aejmelaeus@dynabyte.se>
2014-05-26 22:35:48 +0200

    Commit

---
+++ WebSitesDemo/TestClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSitesDemo
+{
+    public class TestClass
+    {
+        public string HelloPowerShell()
+        {
+            return "Hello PowerShell!";
+        }
+    }
+}
